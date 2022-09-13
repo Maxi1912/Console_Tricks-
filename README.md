59b45051863ef5c8abb2013f2f42f4de16e4eeab
Maxi1912 <maximus.sheva@gmail.com>
2022-09-13 22:30:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-ftf3bq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ftf3bq)
---
+++ script.js
@@ -0,0 +1,10 @@
+const dogs = [
+  { name: 'Snichers', age: 2 },
+  { name: 'hugo', age: 8 },
+];
+
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.colot = '50px';
+}
