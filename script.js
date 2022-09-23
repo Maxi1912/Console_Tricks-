21b49fb85f7dfcb0cbfa476dc49689858c741e8e
Maksym <maximus.sheva@gmail.com>
2022-09-23 19:41:55 +0300

    add description

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 const dogs = [
-  { name: 'Snichers', age: 2 },
-  { name: 'hugo', age: 8 },
+  { name: "Snichers", age: 2 },
+  { name: "hugo", age: 8 },
 ];
 
 function makeGreen() {
-  const p = document.querySelector('p');
-  p.style.color = '#BADA55';
-  p.style.fontSize = '50px';
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
 }
 
-console.log('Hello!');
-console.log("Hello I'm %s", 'Max');
+//1. Regular
+console.log("Hello!");
+
+//2. Interpolated
+console.log("Hello I'm %s", "Max");
+
+//3. Syled
 // console.log('%c Hello I am', 'font-size: 10px; background: red; color: black');
-console.warn('OH NOOO');
-console.error('Shit!');
-console.info('Corcodiles eat 3-4 people pre year');
-console.assert(1 == '1', 'That is wrong!');
+
+//4. Warning!
+console.warn("OH NOOO");
+
+//5. Error
+console.error("Shit!");
+
+//6. Info
+console.info("Corcodiles eat 3-4 people pre year");
+
+//7. Testing
+console.assert(1 == "1", "That is wrong!");
+
+//8. Clearing
 console.clear();
-const p = document.querySelector('p');
+
+//9. Viewing DOM Elements
+const p = document.querySelector("p");
 
 console.log(p);
 console.dir(p);
 
+//10. Grouping together
 dogs.forEach((dog) => {
   console.groupCollapsed(`${dog.name}`);
   console.log(`This is ${dog.name}`);
@@ -29,22 +47,26 @@ dogs.forEach((dog) => {
   console.log(`${dog.name} is ${dog.age * 7} dog years old`);
   console.groupEnd(`${dog.name}`);
 });
-console.count('Wes');
-console.count('Oggi');
-console.count('Wes');
-console.count('Max');
-console.count('Max');
-console.count('Max');
-console.count('Oggi');
-console.count('Max');
-console.count('Max');
 
-console.time('fetching data');
-fetch('https://api.github.com/users/wesbos')
+//11. Counting
+console.count("Wes");
+console.count("Oggi");
+console.count("Wes");
+console.count("Max");
+console.count("Max");
+console.count("Max");
+console.count("Oggi");
+console.count("Max");
+console.count("Max");
+
+//12. Timing
+console.time("fetching data");
+fetch("https://api.github.com/users/wesbos")
   .then((data) => data.json())
   .then((data) => {
-    console.timeEnd('fetching data');
+    console.timeEnd("fetching data");
     console.log(data);
   });
-  
+
+//13. Table
 console.table(dogs);
